063f43b40296bc68e35738d3b639061525980ecd
WeDoCrm <eclues.help@eclues.net>
2013-12-26 02:09:43 +0900

    최초 등록

---
+++ CustomAction/FrmConfig.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace CustomAction
+{
+    partial class FrmConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LabelLicenseFile = new System.Windows.Forms.Label();
+            this.ButtonLicenseFile = new System.Windows.Forms.Button();
+            this.LabelCompanyCd = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CkBoxKeepConfig = new System.Windows.Forms.CheckBox();
+            this.LabelMsg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonNext = new System.Windows.Forms.Button();
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.LabelWarning = new System.Windows.Forms.Label();
+            this.ButtonDBReinstall = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LabelWarning);
+            this.groupBox1.Controls.Add(this.LabelLicenseFile);
+            this.groupBox1.Controls.Add(this.ButtonLicenseFile);
+            this.groupBox1.Controls.Add(this.LabelCompanyCd);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(32, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(540, 336);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            // 
+            // LabelLicenseFile
+            // 
+            this.LabelLicenseFile.AutoSize = true;
+            this.LabelLicenseFile.Location = new System.Drawing.Point(148, 71);
+            this.LabelLicenseFile.Name = "LabelLicenseFile";
+            this.LabelLicenseFile.Size = new System.Drawing.Size(0, 12);
+            this.LabelLicenseFile.TabIndex = 9;
+            // 
+            // ButtonLicenseFile
+            // 
+            this.ButtonLicenseFile.Location = new System.Drawing.Point(23, 65);
+            this.ButtonLicenseFile.Name = "ButtonLicenseFile";
+            this.ButtonLicenseFile.Size = new System.Drawing.Size(122, 24);
+            this.ButtonLicenseFile.TabIndex = 8;
+            this.ButtonLicenseFile.Text = "라이센스 파일열기";
+            this.ButtonLicenseFile.UseVisualStyleBackColor = true;
+            this.ButtonLicenseFile.Click += new System.EventHandler(this.ButtonLicenseFile_Click);
+            // 
+            // LabelCompanyCd
+            // 
+            this.LabelCompanyCd.AutoSize = true;
+            this.LabelCompanyCd.Location = new System.Drawing.Point(90, 100);
+            this.LabelCompanyCd.Name = "LabelCompanyCd";
+            this.LabelCompanyCd.Size = new System.Drawing.Size(99, 12);
+            this.LabelCompanyCd.TabIndex = 7;
+            this.LabelCompanyCd.Text = "회사명(회사코드)";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CkBoxKeepConfig);
+            this.panel1.Controls.Add(this.LabelMsg);
+            this.panel1.Location = new System.Drawing.Point(13, 165);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(401, 108);
+            this.panel1.TabIndex = 6;
+            // 
+            // CkBoxKeepConfig
+            // 
+            this.CkBoxKeepConfig.AutoSize = true;
+            this.CkBoxKeepConfig.Location = new System.Drawing.Point(16, 35);
+            this.CkBoxKeepConfig.Name = "CkBoxKeepConfig";
+            this.CkBoxKeepConfig.Size = new System.Drawing.Size(96, 16);
+            this.CkBoxKeepConfig.TabIndex = 5;
+            this.CkBoxKeepConfig.Text = "기존설정유지";
+            this.CkBoxKeepConfig.UseVisualStyleBackColor = true;
+            // 
+            // LabelMsg
+            // 
+            this.LabelMsg.AutoSize = true;
+            this.LabelMsg.Location = new System.Drawing.Point(8, 11);
+            this.LabelMsg.Name = "LabelMsg";
+            this.LabelMsg.Size = new System.Drawing.Size(251, 12);
+            this.LabelMsg.TabIndex = 4;
+            this.LabelMsg.Text = "이전에 설치된 서버설정을 유지하시겠습니까?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "회사코드:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "WeDo Server 실행에 필요한 라이센스를 인증합니다. \r\n배포받은 라이센스파일을 찾아 선택하세요.";
+            // 
+            // ButtonNext
+            // 
+            this.ButtonNext.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ButtonNext.Location = new System.Drawing.Point(392, 374);
+            this.ButtonNext.Name = "ButtonNext";
+            this.ButtonNext.Size = new System.Drawing.Size(87, 23);
+            this.ButtonNext.TabIndex = 17;
+            this.ButtonNext.Text = "다음 >";
+            this.ButtonNext.UseVisualStyleBackColor = true;
+            this.ButtonNext.Click += new System.EventHandler(this.ButtonNext_Click);
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ButtonCancel.Location = new System.Drawing.Point(484, 374);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(87, 23);
+            this.ButtonCancel.TabIndex = 16;
+            this.ButtonCancel.Text = "취소";
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "ini";
+            this.openFileDialog1.Filter = "WeDo License 파일|*.ini";
+            this.openFileDialog1.SupportMultiDottedExtensions = true;
+            // 
+            // LabelWarning
+            // 
+            this.LabelWarning.AutoSize = true;
+            this.LabelWarning.ForeColor = System.Drawing.Color.OrangeRed;
+            this.LabelWarning.Location = new System.Drawing.Point(27, 124);
+            this.LabelWarning.Name = "LabelWarning";
+            this.LabelWarning.Size = new System.Drawing.Size(349, 24);
+            this.LabelWarning.TabIndex = 10;
+            this.LabelWarning.Text = "WeDo Server가 사용하는 DB는 다음과 같은 포트를 사용합니다.\r\n원하는 포트를 직접 지정하실 수 있습니다.";
+            // 
+            // ButtonDBReinstall
+            // 
+            this.ButtonDBReinstall.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.ButtonDBReinstall.Location = new System.Drawing.Point(299, 374);
+            this.ButtonDBReinstall.Name = "ButtonDBReinstall";
+            this.ButtonDBReinstall.Size = new System.Drawing.Size(87, 23);
+            this.ButtonDBReinstall.TabIndex = 19;
+            this.ButtonDBReinstall.Text = "DB재설치 >";
+            this.ButtonDBReinstall.UseVisualStyleBackColor = true;
+            // 
+            // FrmConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 422);
+            this.Controls.Add(this.ButtonDBReinstall);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ButtonNext);
+            this.Controls.Add(this.ButtonCancel);
+            this.Name = "FrmConfig";
+            this.Text = "WeDo Server 설치 - 라이센스인증";
+            this.Shown += new System.EventHandler(this.FrmConfig_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonNext;
+        private System.Windows.Forms.Button ButtonCancel;
+        private System.Windows.Forms.CheckBox CkBoxKeepConfig;
+        private System.Windows.Forms.Label LabelMsg;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button ButtonLicenseFile;
+        private System.Windows.Forms.Label LabelCompanyCd;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label LabelLicenseFile;
+        private System.Windows.Forms.Label LabelWarning;
+        private System.Windows.Forms.Button ButtonDBReinstall;
+
+    }
+}
---
+++ CustomAction/FrmConfig.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration.Install;
+using System.IO;
+using WeDoCommon;
+using System.Diagnostics;
+
+namespace CustomAction
+{
+    public partial class FrmConfig : Form
+    {
+        System.Configuration.Install.InstallContext formContext = null;
+        InstallController ctrl = null;
+        string companyCd = "";
+        string companyName = "";
+
+        public delegate void DelAppendStatusMsg(string msg);
+        DelAppendStatusMsg delAppendStatusMsg = null;
+
+        public FrmConfig()
+        {
+            InitializeComponent();
+        }
+
+        //폼 우측상단 버튼 안보임
+        private const int WS_SYSMENU = 0x80000;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style &= ~WS_SYSMENU;
+                return cp;
+            }
+        }
+
+        public FrmConfig(System.Configuration.Install.InstallContext context, InstallController ctrl)
+        {
+            formContext = context;
+            this.ctrl = ctrl;
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void FrmConfig_Shown(object sender, EventArgs e)
+        {
+            //wedo가 설치된게 있는지 확인한다.
+            //있는경우 
+            //1.회사코드를 보여주고
+            //2.하단 질문을 보여준다.
+            SetPrevWeDoInfo(ctrl.prevWeDoExists());
+            Initialize();
+        }
+
+        public void SetPrevWeDoInfo(bool exists)
+        {
+            if (exists)
+            {
+                panel1.Visible = true;
+                CkBoxKeepConfig.Checked = true;
+            }
+            else
+            {
+                panel1.Visible = false;
+            }
+        }
+
+        public void Initialize()
+        {
+            ButtonNext.Enabled = false;
+            ButtonDBReinstall.Enabled = false;
+            LabelWarning.Text = "";
+            LabelCompanyCd.Text = "";
+        }
+
+        public bool KeepConfig()
+        {
+            return CkBoxKeepConfig.Checked;
+        }
+
+        /// <summary>
+        /// 회사코드가 변경된 경우는 db재설치를 한경우
+        /// 신규이거나 변경사항 없으면 패스
+        /// 등록한 라이센스파일을 C:\eclues\AppData\config로 복사. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonNext_Click(object sender, EventArgs e)
+        {
+            ctrl.KeepPrevConfig = CkBoxKeepConfig.Checked;
+            if (ctrl.CompanyCd != "" && !ctrl.CompanyCd.Equals(companyCd))
+            {
+                if (MessageBox.Show(string.Format("이전에 사용하던 회사코드({0})와 다른 회사코드({1})가 적용되었습니다. "
+                                                + "\n회사코드를 변경하시겠습니까?"
+                                                 , ctrl.CompanyCd
+                                                 , companyCd)
+                                    , "회사코드 변경"
+                                    , MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                {
+                    DialogResult = DialogResult.None;
+                    ButtonLicenseFile.Focus();
+                    return;
+                }
+            }
+            ctrl.CompanyCd = companyCd;
+            ctrl.CompanyName = companyName;
+            FileInfo info = new FileInfo(LabelLicenseFile.Text);
+            LicenseHandler handler = new LicenseHandler(info);
+            handler.BackupLicenseFile(ConstDef.APP_DATA_CONFIG_DIR);
+            
+        }
+
+        private void ButtonLicenseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new System.Windows.Forms.OpenFileDialog();
+            if (!Directory.Exists(ConstDef.APP_DATA_CONFIG_DIR))
+            {
+                fileDialog.InitialDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            }
+            else
+            {
+                fileDialog.InitialDirectory = ConstDef.APP_DATA_CONFIG_DIR;
+            }
+            fileDialog.DefaultExt = "ini";
+            fileDialog.RestoreDirectory = true;
+            fileDialog.Filter = "WeDo License 파일|*.ini";
+            fileDialog.SupportMultiDottedExtensions = true;
+
+            if (CommonUtil.STAShowDialog(fileDialog) == DialogResult.OK)
+            {
+                Initialize();
+                LabelLicenseFile.Text = fileDialog.FileName;
+                FileInfo info = new FileInfo(LabelLicenseFile.Text);
+                LicenseHandler handler = new LicenseHandler(info);
+                handler.LogWriteHandler += OnStatusWrite;
+
+                handler.ReadLicense();
+
+                companyCd = handler.CompanyCode;
+                companyName = handler.CompanyName;
+                LabelCompanyCd.Text = string.Format("{0}({1})", companyName, companyCd);
+
+                if (ctrl.NeedDbReinstall(handler.CompanyCode))
+                {
+                    ButtonDBReinstall.Enabled = true;
+                    ButtonNext.Enabled = false;
+                    LabelWarning.Text = string.Format("라이센스파일의 회사코드[{0}]가 기존 회사코드[{1}]와 다릅니다.\n"
+                        +" 새로운 회사코드를 쓰려면 DB를 재설치해야 합니다."
+                        , handler.CompanyCode, ctrl.CompanyCd);
+
+                } else {
+                    ButtonDBReinstall.Enabled = false;
+
+
+                    if (handler.ValidLicense())
+                    {
+                        ButtonNext.Enabled = true;
+                        LabelWarning.Text = "라이센스파일이 유효합니다.";
+                    }
+                    else
+                    {
+                        ButtonNext.Enabled = false;
+
+                        LabelWarning.Text = "라이센스파일 검증에 실패했습니다. 파일유효성을 확인하세요.\n";
+
+                        int resultCode = Convert.ToInt16(handler.ResultMessage.Split('&')[0]);
+                        if (resultCode == (int)LicenseResult.ERR_EXPIRED)
+                        {
+                            LabelWarning.Text += "License 만료일자 지남.";
+                        }
+                        else if (resultCode == (int)LicenseResult.ERR_INVALID_FILE)
+                        {
+                            LabelWarning.Text += "잘못된 등록일자, 회사코드/명 또는 라이센스키.";
+                        }
+                        else if (resultCode == (int)LicenseResult.ERR_MAC_ADDR)
+                        {
+                            LabelWarning.Text += string.Format("License file 잘못된 mac address값.실제 mac address[{0}]", handler.MacAddress);
+                        }
+                    }
+                }
+
+            }
+            fileDialog.Dispose();
+        }
+
+        public void OnStatusWrite(object sender, StringEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                Logger.info(e.EventString);
+            });
+        }
+    }
+}
